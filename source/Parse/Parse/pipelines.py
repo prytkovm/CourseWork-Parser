3510c275e6765b491a3a63ab75c13db5c2608947
Maxim Prytkov <maksim.prytkov.2002@gmail.com>
2022-01-04 17:37:18 +0300

    I fucked up

--- source/Parse/Parse/pipelines.py
+++ source/Parse/Parse/pipelines.py
@@ -12,17 +12,25 @@ import csv
 
 class CsvWriterPipeline(object):
 
-    def __init__(self):
-        self.writer = csv.writer(open("D:\\Projects\\Python\\Parse\\items.csv", 'w'), lineterminator='\n', delimiter=',')
+    # path = "D:\\Projects\\Python\\Parse\\items.csv"
+    # file = open(path, "a")
+    # writer = csv.writer(file)
+    # writer.writerow(["name", "price", "store"])
+    # def __init__(self):
+        #self.writer = csv.DictWriter(open("D:\\Projects\\Python\\parse\\items.csv", 'w'), dialect='excel', lineterminator='\n', delimiter=',')
 
     def process_item(self, item, spider):
         self.write_to_csv(item)
         return item
 
     def write_to_csv(self, item):
+        #with open("D:\\Projects\\Python\\Parse\\items.csv", 'a') as CsvFile:
+            # writer = csv.DictWriter(CsvFile, fieldnames=item.keys())
+            # writer.writeheader()
         self.writer.writerow([item['name'], item['price']])
+        #self.writer.writerow([item['name'], item['price']])
 
 
-class ParsePipeline:
-    def process_item(self, item, spider):
-        return item
+# class ParsePipeline:
+#     def process_item(self, item, spider):
+#         return item
